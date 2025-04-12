c0e0a9fccbaa8c486560b7602757cabbea035cd5
DaEpicYoink <tanish.bollam@gmail.com>
2025-04-11 23:29:24 -0400

    update jason data

--- tracks/mock_transactions.py
+++ tracks/mock_transactions.py
@@ -9,49 +9,49 @@ def mock_transactions(account_id):
         {
             "id": "txn1",
             "merchant_id": "food_merchant_id",
-            "description": "Domino's Pizza",
+            "vendor_name": "Domino's Pizza",
             "purchase_date": "2025-04-10",
             "amount": 35.50
         },
         {
             "id": "txn2",
             "merchant_id": "entertainment_id",
-            "description": "Netflix Subscription",
+            "vendor_name": "Netflix Subscription",
             "purchase_date": "2025-04-08",
             "amount": 15.99
         },
         {
             "id": "txn3",
             "merchant_id": "groceries_id",
-            "description": "Trader Joe's",
+            "vendor_name": "Trader Joe's",
             "purchase_date": "2025-04-07",
             "amount": 72.00
         },
         {
             "id": "txn4",
             "merchant_id": "food_merchant_id",
-            "description": "Uber Eats",
+            "vendor_name": "Uber Eats",
             "purchase_date": "2025-04-11",
             "amount": 16.69
         },
         {
             "id": "txn5",
             "merchant_id": "food_merchant_id",
-            "description": "Taco Bell",
+            "vendor_name": "Taco Bell",
             "purchase_date": "2025-04-08",
             "amount": 12.69
         },
         {
             "id": "txn6",
             "merchant_id": "entertainment_id",
-            "description": "Spotfy",
+            "vendor_name": "Spotfy",
             "purchase_date": "2025-04-01",
             "amount": 20.45
         },
         {
             "id": "txn6",
             "merchant_id": "shopping_id",
-            "description": "H&M",
+            "vendor_name": "H&M",
             "purchase_date": "2025-03-15",
             "amount": 35.23
         }
